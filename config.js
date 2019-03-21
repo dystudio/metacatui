bd0716630dbf367a4af183a1131ee7a1c180f177
laurenwalker <walker@nceas.ucsb.edu>
2019-03-21 15:25:56 -0400

    Add cn-stage-2 as the default config option for the CN Base URL, at least for now during development

--- config.js
+++ config.js
@@ -4,5 +4,6 @@
   "root": "http://metacatui.localhost/metacatui",
   "metacatUIVersion": "2.1.0",
   "context" : "/knb",
-  "baseUrl" : "https://dev.nceas.ucsb.edu"
+  "baseUrl" : "https://dev.nceas.ucsb.edu",
+  "cnBaseUrl" : "https://cn-stage-2.test.dataone.org"
 }
